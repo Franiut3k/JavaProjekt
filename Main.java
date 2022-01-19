5cb471ae12d28abc29ac1cacb25de46d91d8957d
Franiut3k <98021589+Franiut3k@users.noreply.github.com>
2022-01-19 22:46:57 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,46 @@
 
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.Node;
+import org.dom4j.*;
 import org.dom4j.io.SAXReader;
+import java.util.HashMap;
 import java.util.List;
 
 import java.io.File;
 
+
+
 public class Main {
-    public static void main(String[] args) throws DocumentException{
-        String path_to_xml = "jordan.esx";
+    public static void main(String[] args) throws DocumentException {
+        File dir = new File("C:\\esx_mml"); // Folder w którym znajdują się pliki esx
+
+        File [] files = dir.listFiles(); // pliki esx
         SAXReader reader = new SAXReader();
-        Document document = reader.read(new File(path_to_xml));
-        List<Node> list_of_nodes = document.selectNodes("//Infix-Term","@absolutepatternMMLId",true);
-        for (int i = 0, size = list_of_nodes.size(); i < size; i++) {
-            Element element = (Element) list_of_nodes.get(i);
-            String attr_value = element.attributeValue("absolutepatternMMLId");
-            String search_xpath = "count(//Infix-Term[@absolutepatternMMLId='"+ attr_value +"'])";
-            Number nodes_amount = document.numberValueOf(search_xpath);
-            System.out.println(attr_value + " | " + nodes_amount.intValue());
+
+        HashMap<String, Integer> attributes_amount = new HashMap<String, Integer>();
+        System.out.println("liczba plików do przetworzenia: "+ files.length);
+        for(File file : files) {
+            if(file.isFile() && file.getName().endsWith(".esx")) {
+                Document document = reader.read(file);
+                List<Node> distinct_attr_nodes = document.selectNodes("//Infix-Term","@absolutepatternMMLId",true);
+                if (distinct_attr_nodes.size()>0){
+                    for (int i = 0, size = distinct_attr_nodes.size(); i < size; i++) {
+                        Element element = (Element) distinct_attr_nodes.get(i);
+                        String attr_value = element.attributeValue("absolutepatternMMLId");
+                        String search_xpath = "count(//Infix-Term[@absolutepatternMMLId='"+ attr_value +"'])";
+                        Integer nodes_amount = document.numberValueOf(search_xpath).intValue();
+
+                        if (attributes_amount.containsKey(attr_value)) {
+                            Integer amount = attributes_amount.get(attr_value);
+                            attributes_amount.put(attr_value, amount + nodes_amount);
+                        }
+                        else {
+                            attributes_amount.put(attr_value, nodes_amount);
+                        }
+                    }
+                }
+            }
         }
+
+        System.out.println(attributes_amount);
     }
 }
