f85085da075343f9a5b0679b5f689431f32ff00a
Franiut3k <98021589+Franiut3k@users.noreply.github.com>
2022-01-23 19:35:57 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -12,35 +12,55 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws DocumentException {
         File dir = new File("C:\\esx_mml"); // Folder w którym znajdują się pliki esx
+        HashMap<String, HashMap> elements = new HashMap<>();
+        elements.put("Infix-Term", new HashMap<String, Integer>());
+        elements.put("Relation-Formula", new HashMap<String, Integer>());
+        elements.put("Circumfix-Term", new HashMap<String, Integer>());
 
         File [] files = dir.listFiles(); // pliki esx
         SAXReader reader = new SAXReader();
 
-        HashMap<String, Integer> attributes_amount = new HashMap<String, Integer>();
-        System.out.println("liczba plików do przetworzenia: "+ files.length);
         for(File file : files) {
-            if(file.isFile() && file.getName().endsWith(".esx")) {
+            String name = file.getName();
+            if(file.isFile() && name.endsWith(".esx") && name.startsWith("a") ) {
                 Document document = reader.read(file);
-                List<Node> distinct_attr_nodes = document.selectNodes("//Infix-Term","@absolutepatternMMLId",true);
-                if (distinct_attr_nodes.size()>0){
-                    for (int i = 0, size = distinct_attr_nodes.size(); i < size; i++) {
-                        Element element = (Element) distinct_attr_nodes.get(i);
-                        String attr_value = element.attributeValue("absolutepatternMMLId");
-                        String search_xpath = "count(//Infix-Term[@absolutepatternMMLId='"+ attr_value +"'])";
-                        Integer nodes_amount = document.numberValueOf(search_xpath).intValue();
+                for (String el : elements.keySet()) {
 
-                        if (attributes_amount.containsKey(attr_value)) {
-                            Integer amount = attributes_amount.get(attr_value);
-                            attributes_amount.put(attr_value, amount + nodes_amount);
+                    HashMap<String, Integer> attributes_amount = elements.get(el);
+                    List<Node> distinct_attr_nodes = document.selectNodes("//"+el,"@absolutepatternMMLId",true);
+                    if (distinct_attr_nodes.size()>0){
+                        for (int i = 0, size = distinct_attr_nodes.size(); i < size; i++) {
+                            Element element = (Element) distinct_attr_nodes.get(i);
+                            String attr_value = element.attributeValue("absolutepatternMMLId");
+                            String search_xpath = "count(//"+el+"[@absolutepatternMMLId='"+ attr_value +"'])";
+                            Integer nodes_amount = document.numberValueOf(search_xpath).intValue();
+
+                            if (attributes_amount.containsKey(attr_value)) {
+                                Integer amount = attributes_amount.get(attr_value);
+                                attributes_amount.put(attr_value, amount + nodes_amount);
+                            }
+                            else {
+                                attributes_amount.put(attr_value, nodes_amount);
+                            }
+
                         }
-                        else {
-                            attributes_amount.put(attr_value, nodes_amount);
-                        }
+                        elements.put(el, attributes_amount);
                     }
+
+
                 }
+                printHash(elements);
             }
         }
 
-        System.out.println(attributes_amount);
     }
+
+    public static void printHash(HashMap<String, HashMap> elements) {
+        for (String el : elements.keySet()) {
+            System.out.println(el);
+            System.out.println(elements.get(el));
+        }
+        System.out.println("\n\n");
+    }
+
 }
